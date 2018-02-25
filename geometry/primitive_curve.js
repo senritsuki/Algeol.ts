73082ef59bc15f20693abcb4bbc311db2a3480d3
senritsuki <senritsuki@gmail.com>
2018-02-26 03:03:26 +0900

    jsファイル除外

--- geometry/primitive_curve.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var cv = require("../algorithm/curve");
-/** 線分 */
-function build_b3_segment(p1, p2, r) {
-    return function (v) { return cv.distance_sp(p1, p2, v) <= r; };
-}
-exports.build_b3_segment = build_b3_segment;
