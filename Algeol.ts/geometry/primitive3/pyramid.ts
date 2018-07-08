5267321625aa21288d6e73846c88e145c6b0f012
senritsuki <senritsuki@gmail.com>
2018-07-08 23:11:24 +0900

    object追加

--- Algeol.ts/geometry/primitive3/pyramid.ts
+++ Algeol.ts/geometry/primitive3/pyramid.ts
@@ -8,26 +8,32 @@ import * as circle from './circle';
 
 /** (底面の頂点数, 底面の外接円の半径, 高さ) -> 角錐の頂点の配列 */
 export function verts_i(n_gonal: number, r: number, h: number): vc.V3[] {
-    const verts: vc.V3[] = [];
-    verts.push(vc.v3(0, 0, h)); // // 頭頂点
-    circle.verts_i(n_gonal, r, 0, 0).forEach(v => verts.push(v)); // 底面
+    // 底面
+    const verts = circle.verts_i(n_gonal, r, 0, 0);
+    // 頭頂点
+    verts.push(vc.v3(0, 0, h));
     return verts;
 }
 /** (底面の頂点数, 底面の内接円の半径, 高さ) -> 角錐の頂点の配列 */
 export function verts_c(n_gonal: number, r: number, h: number): vc.V3[] {
-    const verts: vc.V3[] = [];
-    verts.push(vc.v3(0, 0, h)); // // 頭頂点
-    circle.verts_c(n_gonal, r, 0, 0).forEach(v => verts.push(v)); // 底面
+    // 底面
+    const verts = circle.verts_c(n_gonal, r, 0, 0);
+    // 頭頂点
+    verts.push(vc.v3(0, 0, h));
     return verts;
 }
+
+export function faces_side(n_gonal: number, index_top: number = n_gonal): number[][] {
+    return sq.arithmetic(n_gonal)
+        .map(i => [i, (i + 1) % n_gonal])
+        .map(v => [v[0], v[1], index_top]);
+}
+
 /** (底面の頂点数) -> 角錐の面の配列 */
 export function faces(n_gonal: number): number[][] {
-    const faces: number[][] = [];
     // 側面
-    sq.arithmetic(n_gonal)
-        .map(i => [0, i + 1, (i + 1) % n_gonal + 1])
-        .forEach(v => faces.push(v));
+    const faces = faces_side(n_gonal);
     // 底面
-    faces.push(sq.arithmetic(n_gonal).map(i => i + 1));
+    faces.push(sq.arithmetic(n_gonal));
     return faces;
 }
--- Algeol.ts/geometry/primitive3/star.ts
+++ Algeol.ts/geometry/primitive3/star.ts
@@ -4,10 +4,26 @@
  * Copyright (c) 2016 senritsuki
  */
 
+import * as ut from '../../algorithm/utility';
+import * as sq from '../../algorithm/sequence';
 import * as vc from '../../algorithm/vector';
 import * as circle from '../primitive2/circle';
 
 
-export function verts(n_gonal: number, r1: number, r2: number, z1: number = 0, z2: number = 0): vc.V3[] {
-    return circle.verts(n_gonal, r, rad).map(v => vc.v2_to_v3(v, z));
+export function verts(n_gonal: number, r1: number, r2: number, rad: number = 0, z1: number = 0, z2: number = 0): vc.V3[] {
+    const d = ut.deg180 / n_gonal;
+    const o = vc.v3_zero;
+    const v1 = circle.verts(n_gonal, r1, rad).map(v => vc.v2_to_v3(v, z1));
+    const v2 = circle.verts(n_gonal, r2, rad + d).map(v => vc.v2_to_v3(v, z2));
+    return v1.concat(v2).concat([o]);
+}
+
+export function faces(n_gonal: number): number[][] {
+    const faces: number[][] = [];
+    const io = n_gonal * 2;
+    sq.arithmetic(n_gonal).forEach(i => {
+        faces.push([io, i, i + n_gonal]);
+        faces.push([io, (i+1) % n_gonal, i + n_gonal]);
+    });
+    return faces;
 }
